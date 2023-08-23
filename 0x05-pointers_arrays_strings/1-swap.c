c41bf30e86102139e20f0c29b4010c1e5cccf249
u2045MAWAYI <u20451769@tuks.co.za>
2023-08-22 20:14:53 -0700

    hope

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
-#include "main.h" 
+#include "main.h"
 /**
- * swap_int - swaps the values of two integers 
- * @a: integer to swap 
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
  * @b: integer to swap
  */
 void swap_int(int *a, int *b)
 {
 	int m;
-	m = *a;
+	 m = *a;
 	*a = *b;
 	*b = m;
 }
