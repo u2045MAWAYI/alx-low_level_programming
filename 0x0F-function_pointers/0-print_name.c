9e1b12b194d9f6a11985cfcd07967182b26ef7f0
u2045MAWAYI <u20451769@tuks.co.za>
2023-09-13 03:44:01 -0700

    function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,15 @@
 #include "function_pointers.h"
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * print_name - that prints the name using pointer to function
- * @name: name to print
- * @f: function to use
- */
+ * print_name - print name using pointer's to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,19 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * array_iterator - it will iterates through array
- * @array: array to go through
- * @size: size of array
- * @action: action function to take
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
 	if (array == NULL || action == NULL)
-		exit(98);
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
