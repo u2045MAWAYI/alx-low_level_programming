14ff9e7a805cead9ddef3a798130507a6d862d9a
u2045MAWAYI <u20451769@tuks.co.za>
2024-02-19 01:24:29 -0800

    double link list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: A pointer to the head of the dlistint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_dlistint(const dlistint_t *h) {
     size_t node_count = 0;
 
