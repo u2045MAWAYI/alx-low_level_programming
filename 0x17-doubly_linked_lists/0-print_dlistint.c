a961883afbd2641891578ff6c89d3d79497ae5e9
u2045MAWAYI <u20451769@tuks.co.za>
2024-02-19 01:27:50 -0800

    double link list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,14 +7,16 @@
  *
  * Return: The number of nodes in the list.
  */
-size_t print_dlistint(const dlistint_t *h) {
-    size_t node_count = 0;
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node_count = 0;
 
-    while (h != NULL) {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_count++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_count++;
+	}
 
-    return node_count;
+	return (node_count);
 }
