9f72f37fe41b48f38afd50c89d4d9a72c076a8b1
u2045MAWAYI <u20451769@tuks.co.za>
2024-06-07 02:51:27 -0700

    hashtables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,23 +9,24 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-    unsigned long int i;
+	hash_table_t *table;
+	unsigned long int idx = 0;
 
-    ht = malloc(sizeof(hash_table_t));
-    if (!ht)
-        return (NULL);
+	if (size <= 0)
+		return (NULL);
 
-    ht->size = size;
-    ht->array = malloc(size * sizeof(hash_node_t *));
-    if (!ht->array)
-    {
-        free(ht);
-        return (NULL);
-    }
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-        ht->array[i] = NULL;
-
-    return (ht);
+	while (idx < size)
+		table->array[idx++] = NULL;
+	return (table);
 }
